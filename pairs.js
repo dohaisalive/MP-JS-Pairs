16676cce4eabb77b8a9beea5b0f291bb0c0f2e74
dohaisalive <dohaisalive@gmail.com>
2022-03-21 18:24:52 +0300

    SOLVED

--- pairs.js
+++ pairs.js
@@ -32,16 +32,44 @@
  * console.log(random); // 3 (the random element)
  * console.log(numbers);  // [1, 2, 4] (missing the random element)
  ************************************************/
+ let element1;
+ let element2;
+ let myArray = [];
+
 Array.prototype.getRandom = function () {
   return this.splice(Math.floor(Math.random() * this.length), 1)[0];
 };
 
 function pairs(names) {
   // Your code goes here
+  if (names === [] || names === undefined){
+    myArray = [];
+  } 
+
+  else if (names.length % 2 === 0){
+     myArray = [];
+    while (names.length !== 0){
+      element1 = names.getRandom();
+      element2 = names.getRandom();
+      myArray.push([element1, element2]);
+    }
+  }
+
+  else {
+    while (names.length !== 1){
+      element1 = names.getRandom();
+      element2 = names.getRandom();
+      myArray.push([element1, element2]);
+
+    }
+    myArray.push([names.getRandom()]);
+  }
+    
+  return myArray;
 }
+  
+
 
 module.exports = pairs;
 
-console.log(
-  pairs(["Asis", "Hamsa", "Fawas", "Mishmish", "Hussein", "Lailz", "Mr Potato"])
-);
+//pairs(["Asis", "Hamsa", "Fawas", "Mishmish", "Hussein", "Lailz", "Mr Potato"])
